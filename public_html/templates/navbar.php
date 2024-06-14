625822bbe1577fd4fa269f5ff8e2cf2b75834502
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-06-15 00:07:05 +0200

    poprawa ścieżek

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -1,7 +1,7 @@
   <!-- Pierwszy navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
     <!-- Zastąpienie tekstu logo własnym zdjęciem -->
-    <a class="navbar-brand custom-navbar-brand" href="../www/index.php"><img src="../zdj/stadia_controller_FILL0_wght400_GRAD0_opsz24.png" alt="Logo" style="max-width: 100px;"></a>
+    <a class="navbar-brand custom-navbar-brand" href="index.php"><img src="../zdj/stadia_controller_FILL0_wght400_GRAD0_opsz24.png" alt="Logo" style="max-width: 100px;"></a>
     
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
       
@@ -11,7 +11,7 @@
     <div class="collapse navbar-collapse custom-navbar-nav" id="navbarSupportedContent1">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link custom-nav-link" href="../www/userPage.php">Strona 1</a>
+          <a class="nav-link custom-nav-link" href="userPage.php">Strona 1</a>
         </li>
         <li class="nav-item">
           <a class="nav-link custom-nav-link" href="#">Strona 2</a>
@@ -20,7 +20,7 @@
           <a class="nav-link custom-nav-link" href="#">Strona 3</a>
 
         </li>
-        <li>  <a href="../www/login.php"><img src="../zdj/login_FILL0_wght400_GRAD0_opsz24.png" alt="Logowanie"></a></li>
+        <li>  <a href="login.php"><img src="../zdj/login_FILL0_wght400_GRAD0_opsz24.png" alt="Logowanie"></a></li>
       </ul>
     </div>
     </nav>
