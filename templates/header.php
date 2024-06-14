d0ee968223644091c8ca409870a0032c3166d487
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-06-14 21:55:50 +0200

    zmiana struktury

--- templates/header.php
+++
@@ -1,6 +0,0 @@
-  <!-- Główny obszar -->
-  <header class="jumbotron jumbotron-fluid text-white text-center custom-jumbotron">
-    <div class="container">
-      <h1 class="display-4 custom-jumbotron-heading">TRITON</h1>
-    </div>
-  </header>
