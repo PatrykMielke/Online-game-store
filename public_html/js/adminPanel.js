5a3487c7e023fcfab0beba9afd49f18ca886eb3a
kaczka03 <luczakmarcel03@gmail.com>
2024-08-26 17:20:36 +0200

    xd

--- public_html/js/adminPanel.js
+++ public_html/js/adminPanel.js
@@ -21,3 +21,90 @@ document.querySelectorAll('.btn-danger').forEach(function(button) {
         }
     });
 });
+
+ const userActivityLabels = ['Dzień 1', 'Dzień 2', 'Dzień 3', 'Dzień 4', 'Dzień 5', 'Dzień 6', 'Dzień 7'];
+ const userActivityData = [200, 150, 180, 220, 170, 190, 210];
+ const userActivityLineCtx = document.getElementById('userActivityLineChart').getContext('2d');
+ 
+ new Chart(userActivityLineCtx, {
+     type: 'line',
+     data: {
+         labels: userActivityLabels,
+         datasets: [{
+             label: 'Aktywność',
+             data: userActivityData,
+             borderColor: 'rgba(75, 192, 192, 1)',
+             backgroundColor: 'rgba(75, 192, 192, 0.2)',
+             fill: true
+         }]
+     },
+     options: {
+         scales: {
+             x: {
+                 beginAtZero: true
+             },
+             y: {
+                 beginAtZero: true
+             }
+         }
+     }
+ });
+
+
+ const salesLabels = ['Tydzień 1', 'Tydzień 2', 'Tydzień 3', 'Tydzień 4', 'Tydzień 5', 'Tydzień 6', 'Tydzień 7'];
+ const salesData = [5000, 10000, 15000, 20000, 25000, 30000, 35000];
+ const salesLineCtx = document.getElementById('salesLineChart').getContext('2d');
+ 
+ new Chart(salesLineCtx, {
+     type: 'line',
+     data: {
+         labels: salesLabels,
+         datasets: [{
+             label: 'Sprzedaż',
+             data: salesData,
+             borderColor: 'rgba(153, 102, 255, 1)',
+             backgroundColor: 'rgba(153, 102, 255, 0.2)',
+             fill: true
+         }]
+     },
+     options: {
+         scales: {
+             x: {
+                 beginAtZero: true
+             },
+             y: {
+                 beginAtZero: true
+             }
+         }
+     }
+ });
+
+// User Age Data
+const userAgeLabels = JSON.parse(document.getElementById('userAgeLabels').textContent);
+const userAgeData = JSON.parse(document.getElementById('userAgeData').textContent);
+
+const userAgeBarCtx = document.getElementById('userActivityBarChart').getContext('2d');
+
+new Chart(userAgeBarCtx, {
+    type: 'bar',
+    data: {
+        labels: userAgeLabels,
+        datasets: [{
+            label: 'Liczba użytkowników',
+            data: userAgeData,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            fill: true
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                beginAtZero: true
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
